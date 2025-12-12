d2ef6a44dfac0f1e41d8556f28e8f6f38e262578
elawler0575 <emalinelawler@u.boisestate.edu>
2025-12-11 18:36:32 -0700

    Create game.h

---
+++ FinalProject/game.h
@@ -0,0 +1,17 @@
+/*
+ * game.h
+ *
+ *  Created on: Dec 7, 2025
+ *      Author: emali
+ */
+
+#ifndef SRC_GAME_H_
+#define SRC_GAME_H_
+
+typedef enum{STATE_MENU, STATE_GAME, STATE_ROUND,STATE_OVER} GameState;
+
+extern GameState gameState;
+
+
+
+#endif /* SRC_GAME_H_ */
